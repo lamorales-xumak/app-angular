f8ac877758264ca5115fd9cd69fe7d135184ad6e
Luis Alfredo Morales Franco <ilusion.linux@gmail.com>
2015-09-29 23:04:47 -0600

    Version estable de gulpfile.(bower.json .jshintrc gulpfile.js package.json)

--- gulpfile.js
+++ gulpfile.js
@@ -1,205 +1,218 @@
 'use strict';
-
+//Modulos necesarios############################################################
 var gulp = require('gulp'),
-  connect = require('gulp-webserver'),
-  jshint = require('gulp-jshint'),
-  inject = require('gulp-inject'),
-  wiredep = require('wiredep').stream,
-  gulpif = require('gulp-if'),
-  minify = require('gulp-minify-css'),
-  minhtml = require('gulp-minify-html'),
-  useref = require('gulp-useref'),
-  uglify = require('gulp-uglify'),
-  uncss = require('gulp-uncss'),
-  del = require('del'),
-  cache = require('gulp-ng-template'),
-  imgmin = require('gulp-imagemin'),
-  svgo = require('imagemin-svgo'),
-  png = require('imagemin-optipng'),
-  jpeg = require('imagemin-jpegtran'),
-  gutil = require('gulp-util');
-
+   connect = require('gulp-webserver'),
+   jshint = require('gulp-jshint'),
+   inject = require('gulp-inject'),
+   wiredep = require('wiredep').stream,
+   gulpif = require('gulp-if'),
+   minify = require('gulp-minify-css'),
+   minhtml = require('gulp-minify-html'),
+   useref = require('gulp-useref'),
+   uglify = require('gulp-uglify'),
+   uncss = require('gulp-uncss'),
+   del = require('del'),
+   cache = require('gulp-ng-template'),
+   imgmin = require('gulp-imagemin'),
+   svgo = require('imagemin-svgo'),
+   png = require('imagemin-optipng'),
+   jpeg = require('imagemin-jpegtran'),
+   gutil = require('gulp-util'),
+   jshintfileoutput = require('gulp-jshint-html-reporter');
+//Variables globales############################################################
 var hostServer = '127.0.0.1';
-var puertoLocal = '3595';
-var puertoRemoto = '3599';
-var serverLocal = 'http://127.0.0.1:8080/control-hospitalario/api/v.0.1/';
-var serverRemoto = 'http://app-inventario.azurewebsites.net/control-hospitalario/api/v.0.1/';
-var recurso = '/control-hospitalario/api/v.0.1/';
-
-//DESAROLLO#############################################################
-//Tarea para levantar un servidor de desarrollo local
-gulp.task('server-desarrollo-local', function() {
-  gulp.src('app')
-    .pipe(connect({
-      host: hostServer,
-      port: puertoLocal,
-      livereload: {
-        enable: true,
-        filter: function(filename) {
-          if (filename.match(/.js$/)) {
-            return false;
-          }
-
-          return true;
-        }
-      },
-      fallback: 'index.html',
-      proxies: [{
-        source: recurso
-        target: serverLocal
-      }],
-      open: true
-    }));
-});
-
-gulp.task('server-desarrollo-remoto', function() {
-  gulp.src('app')
-    .pipe(connect({
-      host: hostServer,
-      port: puertoRemoto,
-      livereload: {
-        enable: true,
-        filter: function(filename) {
-          if (filename.match(/.js$/)) {
-            return false;
-          }
-
-          return true;
-        }
-      },
-      fallback: 'index.html',
-      proxies: [{
-        source: recurso,
-        target: serverRemoto
-      }],
-      open: true
-    }));
-});
-
-//Tarea para buscar estilos y javascript en los archivos del proyecto para inyectarlos en pagina principal
-gulp.task('inyeccion', function() {
-  var sources = gulp.src(['./app/js/**/*.js', './app/css/**/*.css', '!./app/css/creador*.css']);
-  return gulp.src('index.html', {
-      cwd: './app'
-    })
-    .pipe(inject(sources, {
-      read: false,
-      ignorePath: '/app'
-    }))
-    .pipe(gulp.dest('./app'));
-});
-
-//Tarea para inyectar las librerias de terceros instaladas mediante bower
-gulp.task('dependencia', function() {
-  return gulp.src('./app/index.html')
-    .pipe(wiredep({
-      directory: './app/lib'
-    }))
-    .pipe(gulp.dest('./app'));
-});
-
-//Tarea para busqueda de errores y mostrarlos en pantalla
-gulp.task('analizar', function() {
-  return gulp.src(['./app/js/**/*.js'])
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(jshint.reporter('fail'));
-});
-
-//Task para convertir las plantillas en templates
-gulp.task('template', function() {
-  return gulp.src('./app/html/**/*.html')
-    .pipe(minhtml())
-    .pipe(cache({
-      prefix: 'html/',
-      moduleName: 'templates',
-      standalone: true,
-      filePath: 'templates.js'
-    }))
-    .pipe(gulp.dest('./app/js'));
-});
-
-
-//Tarea que vigila cambios en los archivos html, css y ejecuta la tarea de actualizacion del navegdor
-gulp.task('vigilar', function() {
-  gulp.watch(['./app/css/**/*.css'], ['inyeccion']);
-  gulp.watch(['./app/js/**/*.js', '!./app/js/templates.js'], ['inyeccion']);
-  gulp.watch(['./app/js/**/*.js', './gulpfile.js'], ['analizar']);
-  gulp.watch(['./app/html/**/*.html'], ['template']);
-  gulp.watch(['./bower.json'], ['dependencia']);
-});
-
-//Tarea default para gulp
+var prtDesaLocal = '3595';
+var prtDesaRemoto = '3599';
+var prtProdLocal = '3695';
+var prtProdRemoto = '3699';
+var serverLocal = 'http://127.0.0.1:8080/xxxx/api/v.0.1/';
+var serverRemoto = 'http://yyyy.azurewebsites.net/xxxx/api/v.0.1/';
+var recurso = '/xxxx/api/v.0.1/';
+//Supertareas###################################################################
 gulp.task('desarrollo', ['server-desarrollo-local', 'vigilar']);
 gulp.task('desarrollo-remoto', ['server-desarrollo-remoto', 'vigilar']);
-//PRODUCCION############################################################
-//Tarea para el servidor de Produccion
-gulp.task('produccion', function() {
-  gulp.src('dist')
-    .pipe(connect({
-      host: '127.0.0.1',
-      port: '9535',
-      livereloadreload: true,
-      fallback: 'index.html',
-      open: true
-    }));
-});
+gulp.task('compilar', ['comprimir']);
+gulp.task('produccion', ['server-produccion-local']);
+gulp.task('produccion-remoto', ['server-produccion-remoto']);
+//DESAROLLO#####################################################################
+//Tarea para levantar un servidor de desarrollo local
+gulp.task('server-desarrollo-local', ['template', 'inyeccion', 'dependencia'], function() {
+   gulp.src('app')
+      .pipe(connect({
+         host: hostServer,
+         port: prtDesaLocal,
+         livereload: {
+            enable: true,
+            filter: function(filename) {
+               if (filename.match(/.js$/)) {
+                  return false;
+               }
 
+               return true;
+            }
+         },
+         fallback: 'index.html',
+         proxies: [{
+            source: recurso
+            target: serverLocal
+         }],
+         open: true
+      }));
+});
+//Tarea para levantar un servidor de desarrollo remoto
+gulp.task('server-desarrollo-remoto', ['template', 'inyeccion', 'dependencia'], function() {
+   gulp.src('app')
+      .pipe(connect({
+         host: hostServer,
+         port: prtDesaRemoto,
+         livereload: {
+            enable: true,
+            filter: function(filename) {
+               if (filename.match(/.js$/)) {
+                  return false;
+               }
+
+               return true;
+            }
+         },
+         fallback: 'index.html',
+         proxies: [{
+            source: recurso,
+            target: serverRemoto
+         }],
+         open: true
+      }));
+});
+//Tarea para buscar estilos y javascript en los archivos del proyecto para inyectarlos en pagina principal
+gulp.task('inyeccion', function() {
+   var sources = gulp.src(['./app/js/**/*.js', './app/css/**/*.css', '!./app/css/creador*.css']);
+   return gulp.src('index.html', {
+         cwd: './app'
+      })
+      .pipe(inject(sources, {
+         read: false,
+         ignorePath: '/app'
+      }))
+      .pipe(gulp.dest('./app'));
+});
+//Tarea para inyectar las librerias de terceros instaladas mediante bower
+gulp.task('dependencia', function() {
+   return gulp.src('./app/index.html')
+      .pipe(wiredep({
+         directory: './app/lib'
+      }))
+      .pipe(gulp.dest('./app'));
+});
+//Tarea para busqueda de errores y mostrarlos en pantalla
+gulp.task('analizar', function() {
+   return gulp.src(['./app/js/**/*.js'])
+      .pipe(jshint('.jshintrc'))
+      .pipe(jshint.reporter('jshint-stylish'))
+      .pipe(jshint.reporter('gulp-jshint-html-reporter', {
+         filename: 'analisis/resultado-analisis.html',
+         createMissingFolders : true
+      }));
+});
+//Task para convertir las plantillas en templates
+gulp.task('template', function() {
+   return gulp.src('./app/html/**/*.html')
+      .pipe(minhtml())
+      .pipe(cache({
+         prefix: 'html/',
+         moduleName: 'templates',
+         standalone: true,
+         filePath: 'templates.js'
+      }))
+      .pipe(gulp.dest('./app/js'));
+});
+//Tarea que vigila cambios en los archivos html, css y ejecuta la tarea de actualizacion del navegdor
+gulp.task('vigilar', function() {
+   gulp.watch(['./app/css/**/*.css'], ['inyeccion']);
+   gulp.watch(['./app/js/**/*.js', '!./app/js/templates.js'], ['inyeccion']);
+   gulp.watch(['./app/js/**/*.js', './gulpfile.js'], ['analizar']);
+   gulp.watch(['./app/html/**/*.html'], ['template']);
+   gulp.watch(['./bower.json'], ['dependencia']);
+});
+//PRODUCCION####################################################################
+//Tarea para el servidor de Produccion Local
+gulp.task('server-produccion-local', ['comprimir']function() {
+   gulp.src('dist')
+      .pipe(connect({
+         host: hostServer,
+         port: prtProdLocal,
+         livereload: false,
+         fallback: 'index.html',
+         proxies: [{
+            source: recurso
+            target: serverLocal
+         }],
+         open: true
+      }));
+});
+//Tarea para el servidor de Produccion Remoto
+gulp.task('server-produccion-remoto', ['comprimir']function() {
+   gulp.src('dist')
+      .pipe(connect({
+         host: hostServer,
+         port: prtProdRemoto,
+         livereload: false,
+         fallback: 'index.html',
+         proxies: [{
+            source: recurso
+            target: serverLocal
+         }],
+         open: true
+      }));
+});
 //Tarea para limpiar directorio dist
 gulp.task('limpiar', function() {
-  return del(['./dist/**/*.*']);
+   return del(['./dist/**/*.*']);
 });
-
 //Tarea para comprimmir los archivos js y css, para luego copiarlos a la carpeta dist
 gulp.task('comprimir', ['copiar'], function() {
-  return gulp.src('./app/index.html')
-    .pipe(useref.assets())
-    .pipe(gulpif('*.js', uglify({
-      mangle: true
-    })).on('error', gutil.log))
-    .pipe(gulpif('*.css', minify()))
-    .pipe(gulp.dest('./dist'));
+   return gulp.src('./app/index.html')
+      .pipe(useref.assets())
+      .pipe(gulpif('*.js', uglify({
+         mangle: true
+      })).on('error', gutil.log))
+      .pipe(gulpif('*.css', minify()))
+      .pipe(gulp.dest('./dist'));
 });
-
 //Tarea para copiar index.html a la carpeta dist
 gulp.task('copiar', ['limpiar'], function() {
-  gulp.src('./app/img/*.svg')
-    .pipe(svgo()())
-    .pipe(gulp.dest('dist/img'));
+   gulp.src('./app/img/*.svg')
+      .pipe(svgo()())
+      .pipe(gulp.dest('dist/img'));
 
-  gulp.src('./app/img/*.png')
-    .pipe(png({
-      optimizationLevel: 3
-    })())
-    .pipe(gulp.dest('dist/img'));
+   gulp.src('./app/img/*.png')
+      .pipe(png({
+         optimizationLevel: 3
+      })())
+      .pipe(gulp.dest('dist/img'));
 
-  gulp.src('./app/img/*.jpg')
-    .pipe(jpeg({
-      progressive: true
-    })())
-    .pipe(gulp.dest('dist/img'));
+   gulp.src('./app/img/*.jpg')
+      .pipe(jpeg({
+         progressive: true
+      })())
+      .pipe(gulp.dest('dist/img'));
 
-  gulp.src('./app/lib/angular-ui-grid/*.{ttf,woff,eof,svg}')
-    .pipe(gulp.dest('./dist/css'));
+   gulp.src('./app/lib/angular-ui-grid/*.{ttf,woff,eof,svg}')
+      .pipe(gulp.dest('./dist/css'));
 
-  return gulp.src('./app/index.html')
-    .pipe(useref())
-    .pipe(minhtml())
-    .pipe(gulp.dest('./dist'));
+   return gulp.src('./app/index.html')
+      .pipe(useref())
+      .pipe(minhtml())
+      .pipe(gulp.dest('./dist'));
 });
-
 //Tarea para quitar css que no se utiliza
 gulp.task('uncss', function() {
-  return gulp.src('./dist/css/style.min.css')
-    .pipe(uncss({
-      html: [
-        './app/index.html',
-        './app/html/**/*.html'
-      ]
-    }))
-    .pipe(minify())
-    .pipe(gulp.dest('./dist/css/style.min.css'));
+   return gulp.src('./dist/css/style.min.css')
+      .pipe(uncss({
+         html: [
+            './app/index.html',
+            './app/html/**/*.html'
+         ]
+      }))
+      .pipe(minify())
+      .pipe(gulp.dest('./dist/css/style.min.css'));
 });
-
-//Tarea para compilar la aplicacion
-gulp.task('compilar', ['comprimir']);
